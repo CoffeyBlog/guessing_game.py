946e5798f4249c630039ed9be6757ce8992d7ad3
coffey <lucascoffey@gmail.com>
2020-07-17 10:29:07 -0400

    adding some logic.

--- guess.py
+++ guess.py
@@ -4,5 +4,26 @@ print("----------------")
 print("My guessing game")
 print("----------------")
 
+
+print("Try to guess the number!")
+print()
+
+
 mm_count = random.randint(1, 100)
 print(mm_count)
+
+
+attempt_limit = 5
+attempts = 0
+
+while attempts < attempt_limit:
+    guess_text = input("What is your guess  ")
+    guess = int(guess_text)
+
+    print(guess)
+
+    attempts += 1
+
+
+
+print("bye")
